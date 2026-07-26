525a9db2ab8427dfe637b7877f749d8953dfa6c8
Patrick Bassner <patrick@bassner.de>
2026-07-26 20:14:34 +0200

    Lecture: expect visibility sync in attachment tests

--- src/test/java/de/tum/cit/aet/artemis/lecture/AttachmentVideoUnitsIntegrationTest.java
+++ src/test/java/de/tum/cit/aet/artemis/lecture/AttachmentVideoUnitsIntegrationTest.java
@@ -73,6 +73,8 @@ class AttachmentVideoUnitsIntegrationTest extends AbstractSpringIntegrationIndep
         irisRequestMockProvider.enableMockingOfRequests();
         irisRequestMockProvider.mockIngestionWebhookRunResponse(dto -> {
         }, ExpectedCount.manyTimes());
+        irisRequestMockProvider.mockLectureUnitVisibilityWebhookRunResponse(dto -> {
+        }, ExpectedCount.manyTimes());
 
         userUtilService.addUsers(TEST_PREFIX, 1, 1, 0, 1);
         this.lecture1 = lectureUtilService.createCourseWithLecture(true);
